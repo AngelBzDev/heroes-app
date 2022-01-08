2ebe55deb785acd86a52bef5f246001ccb313ae8
AngelBzDev <angelbaez.dev@gmail.com>
2022-01-08 00:42:19 -0600

    Nav mejoradi

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, NavLink, useNavigate, useParams } from 'react-router-dom'
 
 import {HiOutlineMenuAlt1} from 'react-icons/hi'
 
@@ -7,7 +7,12 @@ const Navbar = () => {
 
    const navigate = useNavigate()
    const [width] = useState(window.visualViewport.width)
+   const param = window.location.href
    const [showMenu, setShowMenu] = useState(false)
+
+   useEffect(() => {
+      setShowMenu(false)
+   }, [param])
 
    const handleLogout = () => {
       navigate('/login', {
@@ -34,13 +39,14 @@ const Navbar = () => {
                )
             }
          </div>
-         <div className={`navbar_menu ${showMenu && 'oculta'}`}>
+         <div className={`navbar_menu ${!showMenu && 'oculta'}`}>
             <div className={width > 768 ? `navbar__options` : `navbar__options`}>
                <NavLink to="/marvel" className={ ({isActive}) => isActive ? 'active' : ''}>Marvel</NavLink>
-               <NavLink to="/dc" className={ ({isActive}) => isActive ? 'active' : '' }>DC</NavLink>         
+               <NavLink to="/dc" className={ ({isActive}) => isActive ? 'active' : '' }>DC</NavLink> 
+               <NavLink to="/search" className={ ({isActive}) => isActive ? 'active' : '' }>Search</NavLink>       
                <button className='btn-logout' onClick={ handleLogout }>Logout</button>
             </div>
-         </div>         
+         </div>
       </nav>
    )
 }
