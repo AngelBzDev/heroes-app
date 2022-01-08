83448335f86921e3ae0db3b9afad4d488c07abbc
AngelBzDev <angelbaez.dev@gmail.com>
2022-01-08 00:42:55 -0600

    Hook para manejo de formularios

---
+++ src/hooks/useForm.js
@@ -0,0 +1,26 @@
+//Hook que guarda los valores de un input en un objeto
+
+import { useState } from "react"
+
+const useForm = (init = {}) => {
+   const [values, setValues] = useState(init)
+
+   /**
+    * 
+    * @param {target} referencia al objeto en el cual se lanzo el evento
+    */
+   const handleInputChange = ({target}) => {
+      setValues({
+         //Se usa spreed operator para mantener los valores anteriores
+         ...values,
+         [target.name]: target.value
+      })
+   }
+
+   return [
+      values, 
+      handleInputChange
+   ]
+}
+
+export default useForm
